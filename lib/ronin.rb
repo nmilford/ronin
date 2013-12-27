b395754b1d3cb94c219b96245d6780556a3fc9f9
Nathan Milford <nmilford@shutterstock.com>
2013-12-27 18:25:26 -0500

    name init error

--- lib/ronin.rb
+++ lib/ronin.rb
@@ -13,10 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+require 'ronin/config'
 require 'ronin/module_runner'
 require 'ronin/run_list'
 require 'ronin/version'
-require 'ronin/config'
 require 'ronin/puppet'
 require 'ronin/ronin'
 require 'ronin/chef'
--- lib/ronin/ronin.rb
+++ lib/ronin/ronin.rb
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+$LOAD_PATH << "."
 require 'ronin/module_runner'
 require 'ronin/run_list'
 require 'ronin/config'
