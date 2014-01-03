7eac7f7d4f26a629836816a3ba0bd871ec5df622
Nathan Milford <nmilford@shutterstock.com>
2014-01-03 00:40:00 -0500

    added new gem deps for dev

--- spec/lib/run_list_spec.rb
+++ spec/lib/run_list_spec.rb
@@ -2,7 +2,14 @@
 #require 'ronin/run_list'
 
 #describe Ronin::RunList do
-
+#  context "from file" do
+#      let(:burger) { Burger.new(:ketchup => true) }
+#      before  { burger.apply_ketchup }
+#
+#      it "sets the ketchup flag to true" do
+#        burger.has_ketchup_on_it?.should be_true
+#      end
+#    end
 #  before do
 #    Ronin::Config[:etcd_host] = '127.0.0.1'
 #    Ronin::Config[:etcd_port] = 4422
