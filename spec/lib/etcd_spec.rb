b1783a5ad87ee2af7c50ce376ccbad265df3ae4c
Nathan Milford <nmilford@shutterstock.com>
2014-01-04 22:38:27 -0500

    re-enable ruby 1.8.7 tests

--- spec/lib/etcd_spec.rb
+++ spec/lib/etcd_spec.rb
@@ -1,3 +1,18 @@
+# Author:: Nathan Milford (<nathan@milford.io>)
+# Copyright:: Copyright (c) 2013 Nathan Milford
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 require 'spec_helper'
 require 'ronin/etcd'
 require 'ronin/cache'
@@ -38,7 +53,7 @@ describe Ronin::Etcd do
       end
     end
 
-    it "..get_config" do
+    it ".get_config" do
       begin
         Ronin::Etcd.get_config
       rescue SystemExit=>e
